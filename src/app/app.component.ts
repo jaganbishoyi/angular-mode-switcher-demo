8c80bfd9a924f1317290fc4eadac533aced5753f
Jagan Mohan Bishoyi <jaganmohanbishoyi@gmail.com>
2024-08-21 16:36:37 +0530

    Demo updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,19 @@ import { Component } from '@angular/core';
 import { IConfig } from 'ng-mode-switcher'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-light-dark-mode';
+    title = 'angular-light-dark-mode';
 
-  config: IConfig = {
-    legend: {
-      visible: true,
-      // LIGHT: "light",
-      // DARK: "dark",
-      SYSTEM: "auto"
-    }
-  };
+    config: IConfig = {
+        legend: {
+            visible: true,
+            SYSTEM: "auto"
+        }
+    };
 
-  constructor() {
-
-  }
+    constructor() { }
 }
